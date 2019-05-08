397856b23e67a2337a9172b09ddcf0eb21536a47
Diego Catalano <diego.catalano@live.com>
2019-05-07 22:18:07 -0300

    Singleton
    Singleton

---
+++ JDesignPatterns/src/dp/creational/singleton/Singleton.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dp.creational.singleton;
+
+/**
+ * Singleton design pattern.
+ * Some examples useful
+ * https://www.journaldev.com/1377/java-singleton-design-pattern-best-practices-examples
+ * @author Diego Catalano
+ */
+public class Singleton {
+    
+    private int attribute1;
+    private int attribute2;
+
+    public int getAttribute1() {
+        return attribute1;
+    }
+
+    public void setAttribute1(int attribute1) {
+        this.attribute1 = attribute1;
+    }
+
+    public int getAttribute2() {
+        return attribute2;
+    }
+
+    public void setAttribute2(int attribute2) {
+        this.attribute2 = attribute2;
+    }
+    
+    private Singleton(){}
+    
+    public static Singleton getInstance(){
+        return new Singleton();
+    }
+    
+}
