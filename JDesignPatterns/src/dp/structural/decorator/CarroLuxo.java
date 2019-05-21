12786f0a19c680f4af7d1d66445a53cfa8d69e02
Mateus Araujo <mateuscruz22@gmail.com>
2019-05-21 16:00:26 -0300

    Decorator

---
+++ JDesignPatterns/src/dp/structural/decorator/CarroLuxo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dp.structural.decorator;
+
+/**
+ *
+ * @author Mateus Araújo
+ */
+public class CarroLuxo extends CarroDecorator {
+
+    public CarroLuxo(ICarro c) {
+        super(c);
+    }
+
+    @Override
+    public void montar() {
+        super.montar();
+        System.out.print(" Adicionando características ao carro luxo.");
+    }
+}
