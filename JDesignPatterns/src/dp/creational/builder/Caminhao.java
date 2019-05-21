c2c04391ebe74369a19335fd9ccefd6a6f9eb34d
João Vitor de Oliveira Camara <joaovitoroliveiracamara@hotmail.com>
2019-05-20 23:49:13 -0300

    Exemplo do padrão criacional Builder

---
+++ JDesignPatterns/src/dp/creational/builder/Caminhao.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newbuildercarro;
+
+/**
+ *
+ * @author joao
+ */
+public class Caminhao {
+    private final ICarroBuilder builder;
+
+    public Caminhao(final ICarroBuilder builder) {
+        this.builder = builder;
+    }
+
+    public Veiculo construtor() {     //usa os metodos de ICarroBuilder
+        return builder.setRodas(8)
+                      .setCor("Preto")
+                      .setTipo("Caminhao")
+                      .build();
+    }
+}
---
+++ JDesignPatterns/src/dp/creational/builder/VeiculoConstrutor.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newbuildercarro;
+
+/**
+ *
+ * @author joao
+ */
+public class VeiculoConstrutor implements ICarroBuilder {
+    private final Veiculo veiculoTemp;
+
+    public VeiculoConstrutor() {
+        veiculoTemp = new Veiculo();
+    }
+    
+    
+
+    @Override
+    public Veiculo build() {        //retorna um objeto do tipo Veiculo com todos os atributos setados.
+        Veiculo carro = new Veiculo();
+        carro.setCor(veiculoTemp.getCor());
+        carro.setRodas(veiculoTemp.getRodas());
+        carro.setTipo(veiculoTemp.getTipo());
+        return carro;
+    }
+
+    @Override
+    public ICarroBuilder setCor(String cor) {
+        veiculoTemp.setCor(cor);
+        return this;
+    }
+
+    @Override
+    public ICarroBuilder setRodas(int rodas) {
+        veiculoTemp.setRodas(rodas);
+        return this;
+    }
+
+    @Override
+    public ICarroBuilder setTipo(String tipo) {
+        veiculoTemp.setTipo(tipo);
+        return this;
+    }
+
+    
+    
+}
