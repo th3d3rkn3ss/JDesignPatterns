541bc558decd36bbaea2ee47f49381142c0edf7f
Iuri Rodrigues <iurijrl17@gmail.com>
2019-05-22 11:04:42 -0300

    Facade

---
+++ JDesignPatterns/src/dp/structural/Facade/DiscoRigido.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dp.structural.Facade;
+
+/**
+ *
+ * @author iurij
+ */
+//Exemplo de uma classe DiscoRigido que faz a leitura de um dado.
+
+public class DiscoRigido {
+    public void leDado() { 
+    }
+}
---
+++ JDesignPatterns/src/dp/structural/Facade/UnidadeCentralDeProcessamento.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dp.structural.Facade;
+
+/**
+ *
+ * @author iurij
+ */
+//Exemplo de uma classe UnidadeCentralDeProcessamento que processa um dado.
+
+public class UnidadeCentralDeProcessamento {
+    public void processaDado() {
+    }
+}
